0ad69a643c5feea2ac89a888d585bd72c36e7545
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-11 20:36:16 +0300

    change detail

--- views/product.py
+++ views/product.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, jsonify
 from werkzeug.exceptions import BadRequest
+from models import Product
 
 product_app = Blueprint("product_app", __name__)
 
@@ -12,20 +13,23 @@ PRODUCTS = {
 
 @product_app.route("/")
 def product_list():
-    return render_template("products/index.html", products=PRODUCTS)
+    product = Product.query.all()
+    for row in Product.query.all():
+        print(row.name, row.is_new)
+    return render_template("products/index.html", products=product)
 
 
 @product_app.route("/<int:product_id>/", methods=['GET', 'DELETE'])
 def product_detail(product_id: int):
-    try:
-        product_name = PRODUCTS[product_id]
-    except KeyError:
+    product = Product.query.filter_by(id=product_id).one_or_none()
+    if product is None:
         raise BadRequest(f"Invalid product id #{product_id}")
     if request.method == "DELETE":
         PRODUCTS.pop(product_id)
         return jsonify(ok=True)
     return render_template(
         "products/detail.html",
-        product_id=product_id,
-        product_name=product_name,
+        product=product,
     )
+
+
