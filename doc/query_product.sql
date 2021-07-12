7ae7129fbca94f767e302ae25e42a1a39bcaa068
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-13 00:19:18 +0300

    query

--- doc/query_product.sql
+++ doc/query_product.sql
@@ -1,2 +1,2 @@
-insert into ga(name, is_new) values('TEAM', false), ('SQUASH TEAM', true), ('LVIV SQUASH TEAM', true),
+insert into products(name, is_new) values('TEAM', false), ('SQUASH TEAM', true), ('LVIV SQUASH TEAM', true),
 ('LVIV SQUSH FAMILY', false);
