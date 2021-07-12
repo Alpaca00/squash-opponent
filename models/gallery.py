6446218724acb82732b596631ac8cc0642b54429
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-13 00:01:33 +0300

    add Gallery in __all__, import

---
+++ models/gallery.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from models import db
+
+
+class Gallery(db.Model):
+    __tablename__ = "gallery"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    link = Column(String, nullable=False, unique=True)
