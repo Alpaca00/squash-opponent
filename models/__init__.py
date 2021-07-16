35a387d0ba888e6aea89eabdc0d82dc4f2f4b612
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-16 19:33:58 +0300

    append Character

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,10 @@
 from .database import db
-from .product import Product
+from .product import Product, Character
 from .gallery import Gallery
 
 __al__ = [
     'db',
     'Product',
+    'Character',
     'Gallery',
 ]
