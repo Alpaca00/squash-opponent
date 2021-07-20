245986983676f698670bc8009dc78db00da17630
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-20 14:03:42 +0300

    append User

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,12 @@
 from .database import db
 from .product import Product, Character
 from .gallery import Gallery
+from .user import User
 
 __al__ = [
     'db',
     'Product',
     'Character',
     'Gallery',
+    'User',
 ]
