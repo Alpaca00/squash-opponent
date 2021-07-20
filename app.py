c3a69614a56a87d0d89b3b40b7d65321f29f7ca4
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-20 14:04:27 +0300

    commented

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from flask_migrate import Migrate
 import config
 from views import product_app, gallery_app, video_gallery_app, cart_app
-from models import db
+from models import db, User
 
 app = Flask(__name__)
 app.config.update(
@@ -28,14 +28,39 @@ def index():
     return render_template("base.html", name=name)
 
 
-@app.route("/hello/<string:name>/")
-def home(name=None):
-    if name is None:
-        name = 'World!'
-    return f"<h1>Hello {name}!</h1>"
-
-
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
+    print(post_id)
     return f'Post {post_id}'
 
+#
+# @app.route('/cart/<string:first_name>')
+# def show_data():
+#     first_name = request.form.get('first_name')
+#     print(first_name)
+#     return render_template("base.html")
+
+#
+# @app.route('/cart', methods=['GET', 'POST'])
+# def form_to_json():
+#     data = request.form.to_dict(flat=False)
+#     # return jsonify(data)
+#     return '''<h1>The language value is: {}</h1>
+#                   <h1>The framework value is: {}</h1>'''.format(data)
+#
+#
+# @app.route('/cart/form-example', methods=['GET', 'POST']) #allow both GET and POST requests
+# def form_example():
+#     if request.method == 'POST':  #this block is only entered when the form is submitted
+#         language = request.form.get('language')
+#         framework = request.form['framework']
+#         print(language)
+#         print(framework)
+#         return '''<h1>The language value is: {}</h1>
+#                   <h1>The framework value is: {}</h1>'''.format(language, framework)
+#
+#     return '''<form method="POST">
+#                   Language: <input type="text" name="language"><br>
+#                   Framework: <input type="text" name="framework"><br>
+#                   <input type="submit" value="Submit"><br>
+#               </form>'''
