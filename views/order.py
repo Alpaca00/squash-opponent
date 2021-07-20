3b01bf8ef1d5f9ba31fe2c02f409e68b43ecd438
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-20 14:09:02 +0300

    render order

---
+++ views/order.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+
+order_app = Blueprint("order_app", __name__)
+
+
+@order_app.route("/")
+def order_list():
+    return render_template("order/index.html")
