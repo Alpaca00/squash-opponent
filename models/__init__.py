f9f970cd1069bb9c225e7aadaf902cf9e768490d
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-22 23:56:45 +0300

    append TShirt, Order, User

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,13 @@
 from .database import db
-from .product import Product, TShirt
+from .product import Product, TShirt, Order
 from .gallery import Gallery
-
+from .user import User
 
 __al__ = [
     'db',
     'Product',
     'Gallery',
     'TShirt',
+    'Order',
+    'User',
 ]
