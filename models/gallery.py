235437aed919f7155a1cbdebf07335f5f0e45b6b
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-26 00:48:25 +0300

    no change

--- models/gallery.py
+++ models/gallery.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String
 from models import db
 
 
+# server
 class Gallery(db.Model):
     __tablename__ = "gallery"
     id = Column(Integer, primary_key=True)
