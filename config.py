889a776fdbe15abcc7e59b3f12e3e4e3ed2087b7
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-27 20:56:03 +0300

    fix for doc-compose

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-# import os
-#
-# PG_HOST = os.environ['PG_HOST']
+import os
 
-SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://user:password@0.0.0.0:5432/alpaca"
+PG_HOST = os.environ['PG_HOST']
+
+SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://user:password@{PG_HOST}:5432/alpaca"
