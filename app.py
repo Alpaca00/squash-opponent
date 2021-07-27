2b65b531e990e050aedfe7d14e108810b64c24a9
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-27 20:54:27 +0300

    clean code

--- app.py
+++ app.py
@@ -33,53 +33,13 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 
-
-
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         table_result = TableResult.query.order_by(TableResult.position).all()
-        table_score = TableScore.query.order_by(TableScore.date).all()
+        table_score = TableScore.query.order_by(desc(TableScore.date)).all()
         return render_template("base.html", result=table_result, score=table_score)
     name = "World"
     if request.method == 'POST':
         name = request.form.get('name', 'World')
     return render_template("base.html", name=name)
-
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    print(post_id)
-    return f'Post {post_id}'
-
-#
-# @app.route('/cart/<string:first_name>')
-# def show_data():
-#     first_name = request.form.get('first_name')
-#     print(first_name)
-#     return render_template("base.html")
-
-#
-# @app.route('/cart', methods=['GET', 'POST'])
-# def form_to_json():
-#     data = request.form.to_dict(flat=False)
-#     # return jsonify(data)
-#     return '''<h1>The language value is: {}</h1>
-#                   <h1>The framework value is: {}</h1>'''.format(data)
-#
-#
-# @app.route('/cart/form-example', methods=['GET', 'POST']) #allow both GET and POST requests
-# def form_example():
-#     if request.method == 'POST':  #this block is only entered when the form is submitted
-#         language = request.form.get('language')
-#         framework = request.form['framework']
-#         print(language)
-#         print(framework)
-#         return '''<h1>The language value is: {}</h1>
-#                   <h1>The framework value is: {}</h1>'''.format(language, framework)
-#
-#     return '''<form method="POST">
-#                   Language: <input type="text" name="language"><br>
-#                   Framework: <input type="text" name="framework"><br>
-#                   <input type="submit" value="Submit"><br>
-#               </form>'''
