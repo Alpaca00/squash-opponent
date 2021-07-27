7272df478ba1bf801b50f7665a0e0f0dbe729bd9
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-27 20:38:12 +0300

    add funcs for model of tables

--- static/js/main.js
+++ static/js/main.js
@@ -8,3 +8,60 @@ document.addEventListener("click", function (argument){
 });
 
 
+// need refactoring and optimization
+
+
+// Get the modal
+const modal = document.getElementById("myModal");
+const modal1 = document.getElementById("myModal1");
+const modal2 = document.getElementById("myModal2");
+const modal3 = document.getElementById("myModal3");
+
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+const btn1 = document.getElementById("myBtn1");
+const btn2 = document.getElementById("myBtn2");
+const btn3 = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  modal1.style.display = "none";
+  modal2.style.display = "none";
+  modal3.style.display = "none";
+}
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal || event.target == modal1 || event.target == modal2 || event.target == modal3) {
+    modal.style.display = "none";
+    modal1.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+  }
+}
+
+
