36959f7dc8ff5eacb123097230e71b1f2aa87e8b
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-27 20:41:37 +0300

    render register app

--- views/register.py
+++ views/register.py
@@ -1,8 +1,22 @@
 from flask import Blueprint, render_template, request
+from form import RegisterForm
+from models import UserAccount, db
 
 register_app = Blueprint("register_app", __name__)
 
 
-@register_app.route("/", methods=["GET", "POST", "HEAD"])
-def register_list():
-    return render_template("login/register.html")
+
+@register_app.route('/', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return "the form has been submitted. Success!"
+    if request.method == 'POST':
+        email = form.email.data
+        name = form.name.data
+        password = form.password.data
+        user_account = UserAccount(email=email, name=name, password=password)
+        db.session.add(user_account)
+        db.session.commit()
+        return render_template("login/index.html")
+    return render_template("login/register.html", form=form)
