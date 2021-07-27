812637a1d303083638620e7fd8326db5b8d8aee2
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-27 20:41:17 +0300

    render login app

--- views/login.py
+++ views/login.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from models import UserAccount
 
 login_app = Blueprint("login_app", __name__)
 
@@ -8,8 +9,11 @@ def login():
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
-        print(email, password)
-        return render_template("base.html")
+        user_login = UserAccount.query.filter_by(email=email).one_or_none()
+        if user_login.password == password:
+            user = user_login.name
+            return render_template("base.html", user=user)
+        else:
+            return render_template("login/index.html")
     return render_template("login/index.html")
-
 
