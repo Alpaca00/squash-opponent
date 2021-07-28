2fb387f247729df9659d4d6c6fc30ce7e22bf9ee
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-29 00:21:54 +0300

    add custom cls Validator and declare

--- models/user.py
+++ models/user.py
@@ -1,21 +1,47 @@
+import re
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from flask_validator import Validator, ValidateInteger, ValidateString, ValidateEmail
 from models import db
+from app import logger
+
+
+class ValidatePhone(Validator):
+    REGEX = r'^(?:\+38)?(?:\([0-9]{3}\D)[ .-]?[0-9]{3}[ .-]?[0-9]{2}[ .-]?[0-9]{2}|[0-9]{3}[ .-]?[0-9]{3}[ .-]?[0-9]{2}[ .-]?[0-9]{2}|[0-9]{3}[0-9]{7}$gm'
+
+    def check_value(self, value):
+        if re.findall(self.REGEX, value):
+            logger.info(value)
+            return True
+        else:
+            raise AssertionError("Invalid phone number.")
+
 
 
 class User(db.Model):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
+    full_name = Column(String(255), nullable=False)
     email = Column(String(33), unique=True)
     phone = Column(String, nullable=False, unique=True)
-    address = Column(String, nullable=False)
-    address2 = Column(String, nullable=False, server_default='null')
-    city = Column(String, nullable=False)
-    state = Column(String, nullable=False)
+    address = Column(String(255), nullable=False)
+    address2 = Column(String(255), nullable=False)
+    city = Column(String(255), nullable=False)
+    state = Column(String(255), nullable=False)
     zip_code = Column(Integer, nullable=False)
-    order_id = Column(Integer, ForeignKey('orders.id'))
-    order = db.relationship('Order')
+    order_id = Column(Integer, ForeignKey("orders.id"))
+    order = db.relationship("Order")
+
+
+    @classmethod
+    def __declare_last__(cls):
+        ValidateString(User.full_name)
+        ValidateEmail(User.email)
+        ValidatePhone(User.phone)
+        ValidateString(User.address)
+        ValidateString(User.address2)
+        ValidateString(User.city)
+        ValidateString(User.state)
+        ValidateInteger(User.zip_code)
 
 
 
@@ -26,13 +52,5 @@ class UserAccount(db.Model):
     name = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    logout = Column(Boolean, default=False, server_default='false')
+    logout = Column(Boolean, default=False, server_default="false")
 
-    # @email.setter
-    # def email(self, value):
-    #     if len(value) > self.email.type.length:
-    #         raise Exception("Value too long")
-    #     self.email = value
-    #     arr = [x for x in self.email]
-    #     if '@' not in arr:
-    #         raise Exception("The symbol @ must be in the field")
