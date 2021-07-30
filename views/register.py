7d048d3aa79329c895cdfdc4643c739b84830c35
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-30 22:56:26 +0300

    fix create user

--- views/register.py
+++ views/register.py
@@ -1,22 +1,23 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for, redirect
+from flask_security.utils import hash_password
 from form import RegisterForm
-from models import UserAccount, db
+from models import db, user_datastore
 
 register_app = Blueprint("register_app", __name__)
-
 
 
 @register_app.route('/', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        return "the form has been submitted. Success!"
+        return "The form has been submitted. Success!"
     if request.method == 'POST':
-        email = form.email.data
-        name = form.name.data
-        password = form.password.data
-        user_account = UserAccount(email=email, name=name, password=password)
-        db.session.add(user_account)
+        user_datastore.create_user(
+            email=request.form.get('email'),
+            name=request.form.get('name'),
+            password=hash_password(request.form.get('password')),
+        )
         db.session.commit()
-        return render_template("login/index.html")
+        return redirect(url_for('login_app.login'))
     return render_template("login/register.html", form=form)
+
