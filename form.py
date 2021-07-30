1b1e07a61eb78aaeafae34480eeafe673fdcacae
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-30 22:53:36 +0300

    fix deprecated cls

--- form.py
+++ form.py
@@ -1,8 +1,8 @@
-from flask_wtf import Form, RecaptchaField
+from flask_wtf import RecaptchaField, FlaskForm
 from wtforms import PasswordField, StringField, validators
 
 
-class RegisterForm(Form):
+class RegisterForm(FlaskForm):
     name = StringField('name', [validators.DataRequired(), validators.Length(max=255)])
     password = PasswordField('new password', [validators.DataRequired(),validators.Length(min=8)])
     email = StringField('emailaddress', [validators.DataRequired(), validators.Length(min=6, max=35)])
