37270c2a66d4b00c48f2d861fd1914d53ac3fee7
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-30 22:58:15 +0300

    fix user_account

--- views/user_account.py
+++ views/user_account.py
@@ -1,21 +1,11 @@
-from flask import Blueprint, render_template, request
-from models import UserAccount
+from flask import Blueprint, render_template
+from flask_login import current_user
+
 
 user_account_app = Blueprint("user_account_app", __name__)
 
 
-@user_account_app.route("/<int:user_id>/", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
-def user_account(user_id: int):
-    user = UserAccount.query.filter_by(id=user_id).one_or_none()
-    if request.method == "POST":
-        return render_template("user.html", user=user)
-    else:
-        return render_template("login/index.html")
+@user_account_app.route("/", methods=["GET", "POST"])
+def user_account():
+    return render_template("user.html", cur=current_user)
 
-
-@user_account_app.route("/", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
-def user_account_active():
-    if request.method == "GET":
-        return render_template("user.html")
-    else:
-        return render_template("login/index.html")
