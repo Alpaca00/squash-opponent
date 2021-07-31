00103678a9646d7b8c344af94ac7084af508985e
Alpaca00 <alpaca00tuha@gmail.com>
2021-07-31 22:21:03 +0300

    add inheritance UserMixin to cls

--- models/admin.py
+++ models/admin.py
@@ -1,9 +1,12 @@
-from sqlalchemy import Column, Integer, String
-from models import db
 from flask_login import UserMixin
+from sqlalchemy import Column, Integer, String, Boolean
+from models import db
 
 
-class AdminLogin(db.Model, UserMixin):
+
+class AdminLogin(UserMixin, db.Model):
     __tablename__ = "admin_login"
     id = Column(Integer, primary_key=True)
-    name = Column(String(33), nullable=False)
+    username = Column(String(33), nullable=False, unique=True)
+    password = Column(String(255), nullable=False, unique=True)
+    remember = Column(Boolean, default=False)
