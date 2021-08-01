d698e42abdf63aa87381739f90ce5bfb9440dfef
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-01 20:55:24 +0300

    fix render url to index

--- views/support.py
+++ views/support.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request
+from sqlalchemy import desc
 
-from models import db
+from models import db, TableResult, TableScore
 from models.support import SupportMessage
 
 support_app = Blueprint("support_app", __name__)
@@ -9,12 +10,16 @@ support_app = Blueprint("support_app", __name__)
 @support_app.route("/", methods=["GET", "POST", "HEAD"])
 def support_list():
     if request.method == "POST":
-        question = request.form.get('question')
-        email = request.form.get('email')
-        subject = request.form.get('subject')
-        text = request.form.get('text')
-        message = SupportMessage(question=question, email=email, subject=subject, text=text)
+        table_result = TableResult.query.order_by(TableResult.position).all()
+        table_score = TableScore.query.order_by(desc(TableScore.date)).all()
+        question = request.form.get("question")
+        email = request.form.get("email")
+        subject = request.form.get("subject")
+        text = request.form.get("text")
+        message = SupportMessage(
+            question=question, email=email, subject=subject, text=text
+        )
         db.session.add(message)
         db.session.commit()
-        return render_template("base.html")
+        return render_template("base.html", result=table_result, score=table_score)
     return render_template("support/index.html")
