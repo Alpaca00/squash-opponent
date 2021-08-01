a69adfd7304c3625c402b84d35ad8f6718b8c9f5
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-01 20:53:51 +0300

    support btn locator

--- doc/locators/navbar_locators.py
+++ doc/locators/navbar_locators.py
@@ -3,3 +3,4 @@ from selenium.webdriver.common.by import By
 
 class NavBarLocators:
     btn_photo_gallery = (By.XPATH, "//div[@id='navbarNavDropdown']//a[contains(text(), 'Photo Gallery')]")
+    btn_support = (By.XPATH, "//div[@id='navbarNavDropdown']//a[contains(text(), 'Support')]")
