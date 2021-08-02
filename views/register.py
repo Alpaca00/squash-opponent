f40e4172bb7bf552094b012d1807eb40957c2f45
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-02 21:46:33 +0300

    fix validate form

--- views/register.py
+++ views/register.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template, request, url_for, redirect
+from flask import Blueprint, render_template, request, url_for, redirect, flash, jsonify
 from flask_security.utils import hash_password
 from models.form import RegisterForm
-from models import db, user_datastore
+from models import db, user_datastore, UserAccount
+
+from views.validate_form_registration import ValidateForm, ValidationFormError
 
 register_app = Blueprint("register_app", __name__)
 
@@ -9,15 +11,25 @@ register_app = Blueprint("register_app", __name__)
 @register_app.route('/', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-    if form.validate_on_submit():
-        return "The form has been submitted. Success!"
-    if request.method == 'POST':
-        user_datastore.create_user(
-            email=request.form.get('email'),
-            name=request.form.get('name'),
-            password=hash_password(request.form.get('password')),
-        )
-        db.session.commit()
-        return redirect(url_for('login_app.login'))
-    return render_template("login/register.html", form=form)
+    user_unique_email = UserAccount.query.filter_by(email=form.email.data).one_or_none()
+    if request.method == 'GET':
+        return render_template("login/register.html", form=form)
+    try:
+        if ValidateForm(name=form.name.data, email=form.email.data, password=form.password.data):
+            if user_unique_email is not None:
+                flash('The email is already registered.')
+                return render_template("login/register.html", form=form)
+            if request.method == 'POST':
+                user_datastore.create_user(
+                    email=request.form.get('email'),
+                    name=request.form.get('name'),
+                    password=hash_password(request.form.get('password')),
+                )
+                db.session.commit()
+                flash('Thanks for registering')
+                return redirect(url_for('login_app.login'))
+    except ValidationFormError as err:
+        flash(f'{err}')
+        return render_template("login/register.html", form=form)
+
 
