d2a6878b88978b92480f8a165bd933f0f482f668
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-03 11:23:40 +0300

    add test_can_user_login

--- doc/tests_ui/test_smoke/test_regression/test_user_action.py
+++ doc/tests_ui/test_smoke/test_regression/test_user_action.py
@@ -1,8 +1,10 @@
 import pytest
+from selene import query
 from app import app, db, UserAccount, desc
 from doc.locators.login_page_locators import LoginLocators
 from doc.locators.navbar_locators import NavBarLocators
 from doc.locators.registration_page_locators import RegistrationFormLocators
+from doc.locators.user_account_locators import UserCardLocators
 
 
 @pytest.fixture
@@ -18,6 +20,7 @@ class TestUserAction:
     navbar_locator = NavBarLocators()
     login_locator = LoginLocators()
     register_form_locator = RegistrationFormLocators()
+    user_card_locator = UserCardLocators()
     test_email = "test@gmail.com"
     test_name = "Rino"
     test_password = "qwerty12345"
@@ -38,3 +41,21 @@ class TestUserAction:
         ).element(
             self.register_form_locator.submit_btn
         ).click()
+
+    def test_can_user_login(self, user):
+        user.open("/").element(self.navbar_locator.Action.btn_action).click().element(
+            self.navbar_locator.Action.btn_login
+        ).click()
+        user.element(self.login_locator.email_field).hover().type(
+            self.test_email
+        ).element(self.login_locator.password_field).hover().type(
+            self.test_password
+        ).element(
+            self.login_locator.remember_me_checkbox
+        ).hover().click().element(
+            self.login_locator.submit_login
+        ).click()
+        user_email_on_card = user.element(self.user_card_locator.email_info).get(
+            query.text
+        )
+        assert user_email_on_card == self.test_email
