3b906e8725d0f0b68ccf5308fd5cfddd3a99443a
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-03 11:23:04 +0300

    fix login and add alert error message

--- views/login.py
+++ views/login.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user
 from flask_security.utils import verify_password
 from models import UserAccount
@@ -12,11 +12,18 @@ def login():
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
-        user_login = UserAccount.query.filter_by(email=email).one_or_none()
-        if verify_password(password=password, password_hash=user_login.password):
-            login_user(user_login)
-            return redirect(url_for('user_account_app.user_account'))
+        user_data = UserAccount.query.filter_by(email=email).one_or_none()
+        if user_data is not None:
+            if verify_password(password=password, password_hash=user_data.password):
+                login_user(user_data)
+                return redirect(url_for('user_account_app.user_account'))
+            else:
+                flash_message("Invalid password.")
         else:
-            return render_template("login/index.html")
+            flash_message("Invalid email.")
     return render_template("login/index.html")
 
+
+def flash_message(text):
+    flash(text)
+    return render_template("login/index.html")
