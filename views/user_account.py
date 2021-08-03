a38a30f19ab73c91507fe32063b54f444cdc42e2
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-03 11:24:32 +0300

    fix logout

--- views/user_account.py
+++ views/user_account.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
-from flask_login import current_user
-
+from flask import Blueprint, render_template, jsonify, request, url_for
+from flask_login import current_user, logout_user, login_required
+from werkzeug.utils import redirect
 
 user_account_app = Blueprint("user_account_app", __name__)
 
@@ -9,3 +9,10 @@ user_account_app = Blueprint("user_account_app", __name__)
 def user_account():
     return render_template("user.html", cur=current_user)
 
+
+@user_account_app.route('/login', methods=["GET", "POST"])
+@login_required
+def logout():
+    if request.method == "GET":
+        logout_user()
+        return redirect(url_for('login_app.login'))
