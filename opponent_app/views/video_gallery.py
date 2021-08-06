20a2eaef512c07f60e4788e9a27fd2b0ba96fb93
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-06 21:02:37 +0300

    upload link

--- opponent_app/views/video_gallery.py
+++ opponent_app/views/video_gallery.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
+from opponent_app import db
+from opponent_app.models.video_gallery import VideoGallery
 
 video_app = Blueprint("video_app", __name__)
 
@@ -7,3 +9,22 @@ video_app = Blueprint("video_app", __name__)
 @video_app.route("/")
 def video_list():
     return render_template("video_gallery/index.html")
+
+
+@video_app.route("/upload", methods=["POST", "GET"])
+def upload_link():
+    if request.method == "POST":
+        link = request.form.get("video-link")
+        new_link = VideoGallery(name=get_name_file(link), link=link)
+        db.session.add(new_link)
+        db.session.commit()
+        flash("Successful. Will soon appear in the gallery.")
+        return render_template("video_gallery/index.html")
+
+
+def get_name_file(file: str):
+    if file.startswith('http://') or file.startswith('https://'):
+        return 'Valid'
+    else:
+        return 'Not_valid'
+
