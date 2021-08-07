2dd343160306e4581cdb52ecf1f2a1585ff37e3e
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-07 14:07:36 +0300

    add content

--- opponent_app/models/basic_queries/insert_to_tables.py
+++ opponent_app/models/basic_queries/insert_to_tables.py
@@ -1,11 +1,19 @@
-from app import app
+from opponent_app import create_app
 from opponent_app.models import db, TableResult, TableScore, Player
 
 positions = [1, 2, 3, 4]
-teams = ["Warriors", "Snails", "Anchors", "Gladiators"]
-points = [29, 23, 18, 10]
-matches = [3, 3, 2, 2]
+teams = ["Anchors", "Warriors", "Snails", "Gladiators"]
+points = [30, 29, 23, 14]
+matches = [3, 3, 3, 3]
 teams_compositions = [
+    [
+        """
+1.Середович Юрій
+2.Біляєв Остап
+3.Мендзебровський Валентин
+4.Батршин Олексій
+"""
+    ],
     [
         """
 1.Шнейдер Дмитро
@@ -24,14 +32,6 @@ teams_compositions = [
     ],
     [
         """
-1.Середович Юрій
-2.Біляєв Остап
-3.Мендзебровський Валентин
-4.Батршин Олексій
-"""
-    ],
-    [
-        """
 1.Завінський Ростислав
 2.Томенчук Михайло
 3.Стьопкін Євген
@@ -40,16 +40,16 @@ teams_compositions = [
     ],
 ]
 
-dates = ["06.04.21", "06.19.21", "06.26.21", "07.04.21", "07.24.21"]
-scores = ["10:6", "9:7", "10:6", "12:4", "9:7"]
+dates = ["06.04.21", "06.19.21", "06.26.21", "07.04.21", "07.24.21", "08.01.21"]
+scores = ["10:6", "9:7", "10:6", "12:4", "9:7", "12:4"]
 teamsVS = [
-    ["Snails", "Anchors", "Warriors", "Warriors", "Anchors"],
-    ["Gladiators", "Warriors", "Snails", "Gladiators", "Snails"],
+    ["Snails", "Anchors", "Warriors", "Warriors", "Anchors", "Anchors"],
+    ["Gladiators", "Warriors", "Snails", "Gladiators", "Snails", "Gladiators"],
 ]
 
 
 def table_factory(position, team, point, match, team_composition):
-    with app.app_context():
+    with create_app().app_context():
         r = TableResult(position=position, team=team, point=point, match=match)
         p = Player(full_name=team_composition)
         r.players.append(p)
@@ -59,7 +59,7 @@ def table_factory(position, team, point, match, team_composition):
 
 
 def table_result_factory(date, score, team1, team2):
-    with app.app_context():
+    with create_app().app_context():
         s = TableScore(date=date, team1=team1, team2=team2, score=score)
         db.session.add(s)
         db.session.commit()
@@ -74,7 +74,7 @@ for i in range(0, 4):
         team_composition=teams_compositions[i],
     )
 
-for i in range(0, 5):
+for i in range(0, 6):
     table_result_factory(
         date=dates[i], team1=teamsVS[0][i], team2=teamsVS[1][i], score=scores[i]
     )
