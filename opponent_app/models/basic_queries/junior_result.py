e3721ed92b66e5eb1b046ea2bd81f1754119ad17
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-07 14:07:22 +0300

    content

---
+++ opponent_app/models/basic_queries/junior_result.py
@@ -0,0 +1,45 @@
+juniors_up_to_9 = {
+    "1": "Иван Евгеньевич Тимощук",
+    "2": "Artem Shneyder",
+    "3": "Valeria Sorokroy",
+    "4": "Yehor Sergeevich Matiukhov",
+    "5": "Orysia Havryliak",
+}
+
+juniors_up_to_11 = {
+    "1": "Анна Тарасова",
+    "2": "Daniil Vorona",
+    "3": "Михаил Бабков",
+    "4": "Gleb Doroshenko",
+    "5": "Иван Евгеньевич Тимощук",
+    "6": "Valeria Sorokroy",
+    "7": "Artem Shneyder",
+    "8": "Dasha Getun",
+    "9": "Тимур Сліпчук",
+    "10": "Олексій Сотенський",
+    "11": "Захар Харченко",
+    "12": "Марія Дорофеєва",
+}
+
+juniors_up_to_13 = {
+    "1": "Анна Тарасова",
+    "2": "Dmytro Lomikovskyy",
+    "3": "Тимур Сліпчук",
+    "4": "Максим Вікторович Тодосенко",
+    "5": "Yuliana Denysenko",
+    "6": "Дарія Мельник",
+}
+
+juniors_up_to_15 = {
+    "1": "Roman Shkarupylo",
+    "2": "Danylo Kurylyak",
+    "3": "Егор Щербаков",
+    "4": "Oleksa Khita",
+    "5": "Артем Смоляков",
+    "6": "Діана Назаріївна Байовська",
+    "7": "Danylo Charczenko",
+    "8": "Максим Харчук",
+    "9": "Андріана Зайльо",
+    "10": "Damian Buryn",
+    "11": "Yuliana Denysenko",
+}
