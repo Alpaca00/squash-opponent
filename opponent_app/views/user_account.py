97530c6d09b356f7502cccec0b8caa4dfe309033
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-13 12:55:19 +0300

    add history

--- opponent_app/views/user_account.py
+++ opponent_app/views/user_account.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template, jsonify, request, url_for, g
+import calendar
+from datetime import datetime
+
+import humanize
+from flask import Blueprint, render_template, jsonify, request, url_for, g, redirect
 from flask_login import current_user, logout_user, login_required
-from werkzeug.utils import redirect
-from opponent_app.models import db, UserOpponent
+from opponent_app.models import db, UserOpponent, UserAccount
 
 user_account_app = Blueprint("user_account_app", __name__)
 
@@ -18,6 +21,19 @@ def pull_lang_code(endpoint, values):
 
 @user_account_app.route("/", methods=["GET", "POST"])
 def user_account():
+    if request.method == "GET":
+        user_history = UserAccount.query.filter_by(id=current_user.id).join(UserOpponent).all()
+        lst_history = []
+        for x in user_history:
+            for i in x.users_opponent:
+                convert_dt = datetime.strptime(i.date, "%Y-%m-%dT%H:%M")
+                abr_month = calendar.month_abbr[int(convert_dt.date().month)]
+                day_ = humanize.naturaldate(convert_dt.day)
+                time_ = humanize.naturaltime(convert_dt.time())
+                lst_history.append(
+                    [abr_month, day_, time_, i.city, i.district, i.category, i.phone]
+                )
+        return render_template("user.html", cur=current_user, opponents=user_history, dates=lst_history)
     if request.method == "POST":
         date_time = request.form.get("partydate")
         category = request.form.get("category")
