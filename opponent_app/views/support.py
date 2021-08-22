4c8b5f03e1c5a55559aaea10c1f230cd2cc31fdd
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-22 17:14:11 +0300

    add func 'send_support_data'

--- opponent_app/views/support.py
+++ opponent_app/views/support.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template, request, g
-from sqlalchemy import desc
-from opponent_app.models import db, TableResult, TableScore
+from flask import Blueprint, render_template, request, g, url_for, redirect
+from opponent_app.models import db
 from opponent_app.models.support import SupportMessage
+from opponent_app.helpers import mail, mail_settings, Message
 
 support_app = Blueprint("support_app", __name__)
 
@@ -19,8 +19,6 @@ def pull_lang_code(endpoint, values):
 @support_app.route("/", methods=["GET", "POST", "HEAD"])
 def support_list():
     if request.method == "POST":
-        table_result = TableResult.query.order_by(TableResult.position).all()
-        table_score = TableScore.query.order_by(desc(TableScore.date)).all()
         question = request.form.get("question")
         email = request.form.get("email")
         subject = request.form.get("subject")
@@ -30,6 +28,15 @@ def support_list():
         )
         db.session.add(message)
         db.session.commit()
-         # send_list_to_mail
-        return render_template("base.html", result=table_result, score=table_score)
+        send_support_data(subject=subject, body=f"email: {email}\nquestion: {question}\nmessage: {text}")
+        return redirect(url_for("home_app.index"))
     return render_template("support/index.html")
+
+
+def send_support_data(subject, body):
+    msg = Message(subject=subject,
+                  sender=mail_settings.get("MAIL_USERNAME"),
+                  recipients=["squashopponent@gmail.com"],
+                  body=body
+                  )
+    return mail.send(msg)
