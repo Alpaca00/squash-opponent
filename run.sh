1fdae9d5ddab5ebb683657fac6f427852c07eb2e
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-25 16:01:46 +0300

    deploy

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+export SPS="Mozi2229659Ozi" export SECRET_KEY="6Lf0rL8bAAAAAL0YqesYius-y0iQnYThoR-RWd0s" export EMAIL_USERNAME="squashopponent@gmail.com" export EMAIL_PASSWORD="OM2229659"
 FLASK_ENV=development FLASK_APP="opponent_app:create_app" flask run --host=0.0.0.0
