b4ce8da8fb59282608a7e9cb95728e948d55a42e
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-25 16:14:15 +0300

    deploy

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 export SPS="Mozi2229659Ozi" export SECRET_KEY="6Lf0rL8bAAAAAL0YqesYius-y0iQnYThoR-RWd0s" export EMAIL_USERNAME="squashopponent@gmail.com" export EMAIL_PASSWORD="OM2229659"
+docker-compose up
 FLASK_ENV=development FLASK_APP="opponent_app:create_app" flask run --host=0.0.0.0
