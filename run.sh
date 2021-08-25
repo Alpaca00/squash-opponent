1378c81027f12a53ee43fd46fd52bcdeac5debfd
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-25 19:15:04 +0300

    gunicorn web

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 export SPS="Mozi2229659Ozi" export SECRET_KEY="6Lf0rL8bAAAAAL0YqesYius-y0iQnYThoR-RWd0s" export EMAIL_USERNAME="squashopponent@gmail.com" export EMAIL_PASSWORD="OM2229659"
-docker-compose up
-FLASK_ENV=development FLASK_APP="opponent_app:create_app" flask run --host=0.0.0.0
+
+#FLASK_ENV=development FLASK_APP="opponent_app:create_app" flask run --host=0.0.0.0
+
+gunicorn -b 0.0.0.0:8000 wsgi:app
+
