c2a96f114fc725eb1289d9e619ea3997b52dca47
Alpaca00 <alpaca00tuha@gmail.com>
2021-08-27 12:48:59 +0300

    for local

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
-export SPS="Mozi2229659Ozi" export SECRET_KEY="6Lf0rL8bAAAAAL0YqesYius-y0iQnYThoR-RWd0s" export EMAIL_USERNAME="squashopponent@gmail.com" export EMAIL_PASSWORD="OM2229659"
+export SPS="Mozi2229659Ozi"
+export SECRET_KEY="6Lf0rL8bAAAAAL0YqesYius-y0iQnYThoR-RWd0s"
+export EMAIL_USERNAME="squashopponent@gmail.com"
+export EMAIL_PASSWORD="OM2229659"
 
 #FLASK_ENV=development FLASK_APP="opponent_app:create_app" flask run --host=0.0.0.0
-
+echo production version
+#echo run wsgi app heroku
 #gunicorn -b 0.0.0.0:8000 wsgi:app
 gunicorn wsgi:app
 
