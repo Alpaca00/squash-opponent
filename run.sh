7059502605d855e5a2c6f184f35ba68a9816718a
Alpaca00 <alpaca00tuha@gmail.com>
2021-09-04 20:29:25 +0300

    test flask run

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@ export SECRET_KEY="6Lf0rL8bAAAAAL0YqesYius-y0iQnYThoR-RWd0s"
 export EMAIL_USERNAME="squashopponent@gmail.com"
 export EMAIL_PASSWORD="OM2229659"
 
-#FLASK_ENV=development FLASK_APP="opponent_app:create_app" flask run --host=0.0.0.0
+#sudo FLASK_ENV=development FLASK_APP="opponent_app:create_app" flask run --host=0.0.0.0 --port=80
 echo production version
 #echo run wsgi app heroku
-#gunicorn -b 0.0.0.0:8000 wsgi:app
+#gunicorn 0.0.0.0:8000 wsgi:app
 gunicorn wsgi:app
+#gunicorn -w 1 -b 172.25.0.1:8000 wsgi:app
 
