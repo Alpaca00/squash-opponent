6b824c5ed9230888b083756a58085b232e716c86
Alpaca00 <alpaca00tuha@gmail.com>
2021-09-11 13:19:08 +0300

    test jenpy

--- tests/tests_ui/test_smoke/test_send_offer_to_opponent.py
+++ tests/tests_ui/test_smoke/test_send_offer_to_opponent.py
@@ -8,16 +8,16 @@ class TestFinderOpponent:
     navbar_locator = NavBarLocators()
     finder_opponent_locator = FinderOpponentLocators()
 
-    # def test_click_on_finder_opponent(self, user):
-    #     user.open("/").element(self.navbar_locator.btn_finder_opponent).hover().should(
-    #         have.exact_text("FINDER-OPPONENT")
-    #     )
-    #     user.open("/").element(self.navbar_locator.btn_finder_opponent).click()
-    #     assert user.driver.title == "Finder-opponent"
+    def test_click_on_finder_opponent(self, user):
+        user.open("/").element(self.navbar_locator.btn_finder_opponent).hover().should(
+            have.exact_text("FINDER-OPPONENT")
+        )
+        user.open("/").element(self.navbar_locator.btn_finder_opponent).click()
+        assert user.driver.title == "Finder-opponent"
 
     def test_make_an_offer_if_already_made(self, user):
         user.open("/en/finder").all(self.finder_opponent_locator.btns_offer)[0].click()
         alert = user.config.driver.switch_to.alert
-        assert alert.text == "You need to wait for the answer of the opponent on the last offer   ."
+        assert alert.text == "You need to wait for the answer of the opponent on the last offer."
         alert.accept()
         assert user.all(self.finder_opponent_locator.btns_offer)[0].should(be.visible)
