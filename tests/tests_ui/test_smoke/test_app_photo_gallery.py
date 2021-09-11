bd9bd02dea48a5371c4739a7943e2b165ebc71d8
Alpaca00 <alpaca00tuha@gmail.com>
2021-09-11 14:47:00 +0300

    fix tests

--- tests/tests_ui/test_smoke/test_app_photo_gallery.py
+++ tests/tests_ui/test_smoke/test_app_photo_gallery.py
@@ -8,16 +8,18 @@ class TestPhotoGallery:
     navbar_locator = NavBarLocators()
     photo_gallery_locator = PhotoGalleryLocators()
 
+    @pytest.mark.create_image
     def test_click_on_photo_gallery(self, user):
         user.open("/").element(self.navbar_locator.btn_photo_gallery).hover().should(
-            have.exact_text("Photo Gallery")
+            have.exact_text("PHOTO-GALLERY")
         )
         user.open("/").element(self.navbar_locator.btn_photo_gallery).click()
         assert user.driver.title == "Gallery"
 
+    @pytest.mark.create_image
     @pytest.mark.parametrize("item", [0, 2, 4])
     def test_click_on_image_and_wait_for_the_modal_window(self, user, item):
-        user.open("/gallery").all(self.photo_gallery_locator.lst_images)[item].click()
+        user.open("/en/gallery").all(self.photo_gallery_locator.lst_images)[item].click()
         user.element(self.photo_gallery_locator.modal_window).should(be.visible)
         model_window_title = user.element(
             self.photo_gallery_locator.modal_window_title
