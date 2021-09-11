f123cd0ecbc9ad60ec403754d1e53534bdeb5dd6
Alpaca00 <alpaca00tuha@gmail.com>
2021-09-11 15:21:58 +0300

    fix xpath loc

--- tests/locators/navbar_locators.py
+++ tests/locators/navbar_locators.py
@@ -2,7 +2,8 @@ from selenium.webdriver.common.by import By
 
 
 class NavBarLocators:
-    btn_photo_gallery = (By.XPATH, "//div[@id='navbarNavDropdown']//a[contains(text(), 'Photo Gallery')]")
+    btn_photo_gallery = (By.XPATH, "//a[contains(text(), 'Photo-Gallery')]")
+    btn_finder_opponent = (By.XPATH, "//a[contains(text(), 'Finder-Opponent')]")
     btn_support = (By.XPATH, "//div[@id='navbarNavDropdown']//a[contains(text(), 'Support')]")
 
     class Action:
