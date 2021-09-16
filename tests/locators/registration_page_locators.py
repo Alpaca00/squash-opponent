f13aa0f493c21c2262232e9ed19a72e994d7c19f
Alpaca00 <alpaca00tuha@gmail.com>
2021-09-16 21:16:31 +0300

    add cls ConfirmPageLocators

--- tests/locators/registration_page_locators.py
+++ tests/locators/registration_page_locators.py
@@ -5,4 +5,7 @@ class RegistrationFormLocators:
     email_field = (By.XPATH, "//*[@id='email-id']")
     name_field = (By.XPATH, "//*[@id='name-id']")
     password_field = (By.XPATH, "//*[@id='password-id']")
-    submit_btn = (By.XPATH, "//button[contains(text(), 'submit')]")
+    submit_btn = (By.XPATH, "//button[contains(text(), 'SUBMIT')]")
+
+    class ConfirmPageLocators:
+        account_confirm_link = (By.XPATH, "//a[@href='/en/register/resend']")
