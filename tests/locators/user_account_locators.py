48dc1fe0b0e74e52bfcb63b386d24e4a081f6b29
Alpaca00 <alpaca00tuha@gmail.com>
2021-09-17 17:16:58 +0300

    add 2 cls locators

--- tests/locators/user_account_locators.py
+++ tests/locators/user_account_locators.py
@@ -3,3 +3,14 @@ from selenium.webdriver.common.by import By
 
 class UserCardLocators:
     email_info = (By.XPATH, "//div[@id='user-card-email']/h6")
+    phone_field = (By.XPATH, "//*[@id='user-phone']")
+    location_select = (By.XPATH, "//select[@id='location-select']")
+    category_select = (By.XPATH, "//select[@id='category-select']")
+    date_field = (By.XPATH, "//input[@id='party']")
+    post_btn = (By.XPATH, "//input[@class='post-btn']")
+
+    class History:
+        all_delete_post_btn = (By.XPATH, "//a[@id='delete-post-opponent']")
+
+    class DisplayAlertInfo:
+        alert_info = (By.XPATH, "//*[@class='alert alert-info']")
