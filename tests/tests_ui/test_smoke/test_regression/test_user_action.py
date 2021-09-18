7d3ff26f5c376eece8af8071c26c7bbe638ed5fd
Alpaca00 <alpaca00tuha@gmail.com>
2021-09-18 14:04:53 +0300

    move env

--- tests/tests_ui/test_smoke/test_regression/test_user_action.py
+++ tests/tests_ui/test_smoke/test_regression/test_user_action.py
@@ -24,6 +24,9 @@ def clean_user_account_db(app):
         db.session.commit()
 
 
+USER_PASSWORD = os.environ["USER_PASSWORD"]
+
+
 class TestUserAction:
     navbar_locator = NavBarLocators()
     login_locator = LoginLocators()
@@ -33,7 +36,6 @@ class TestUserAction:
     test_name = "Test"
     test_password = "qwerty12345"
     actual_user_email = "alpaca00tuha@gmail.com"
-    USER_PASSWORD = os.environ["USER_PASSWORD"]
     phone_user = "+380677667776"
     optimal_date = "2021-09-20T18:00"
 
@@ -115,7 +117,7 @@ class TestUserAction:
             ).should(have.exact_text("Resend"))
 
     def test_if_already_confirmed_user(self, user):
-        self.user_login(user=user, email=self.actual_user_email, password=self.USER_PASSWORD)
+        self.user_login(user=user, email=self.actual_user_email, password=USER_PASSWORD)
         assert (
             user.all(self.user_card_locator.email_info[1])[0]
             .hover()
@@ -128,7 +130,7 @@ class TestUserAction:
             pytest.skip("Not the English version of the site.", allow_module_level=True)
         else:
             self.user_login(
-                user=user, email=self.actual_user_email, password=self.USER_PASSWORD
+                user=user, email=self.actual_user_email, password=USER_PASSWORD
             )
             before_quantity_post = user.all(
                 self.user_card_locator.History.all_delete_post_btn
