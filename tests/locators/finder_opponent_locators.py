f63b9c7a62107444fa88164b87826caa57e89de0
Alpaca00 <alpaca00tuha@gmail.com>
2021-09-21 17:03:57 +0300

    add cls: OfferModalWindow, MessageWidget

--- tests/locators/finder_opponent_locators.py
+++ tests/locators/finder_opponent_locators.py
@@ -5,3 +5,18 @@ class FinderOpponentLocators:
     btns_offer = (By.XPATH, "//*[@id='offer-show-modal']")
     alert_offer = (By.XPATH, "//*[@id='alert-offer']")
     alert_accept = (By.XPATH, "//*[@id='alert-accept']")
+
+    class OfferModalWindow:
+        label = (By.XPATH, "//*[@id='exampleModalLabel']")
+        name_field = (By.XPATH, "//input[@id='user-name']")
+        email_field = (By.XPATH, "//input[@id='user-email']")
+        phone_field = (By.XPATH, "//input[@id='user-phone']")
+        location_select = (By.XPATH, "//select[@id='location-select']")
+        category_select = (By.XPATH, "//select[@id='category-select']")
+        message_text = (By.XPATH, "//*[@id='message-text']")
+        date_picker = (By.XPATH, "//input[@id='party']")
+        send_btn = (By.XPATH, "//input[@id='blockBtn']")
+
+    class MessageWidget:
+        label = (By.XPATH, "//div[@class='pop-up-content-wrap']/h6")
+        text_message = (By.XPATH, "//div[@class='pop-up-content-wrap']/p")
