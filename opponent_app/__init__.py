ac084eb82042856d5e26addcaa6655803373daba
Alpaca00 <alpaca00tuha@gmail.com>
2021-09-25 00:34:24 +0300

    dev

--- opponent_app/__init__.py
+++ opponent_app/__init__.py
@@ -38,7 +38,7 @@ from opponent_app.views import (
 )
 
 
-def create_app(environment_name="production"):
+def create_app(environment_name="development"):
     app = Flask(__name__)
     app.config.from_object(configurations[environment_name])
 
