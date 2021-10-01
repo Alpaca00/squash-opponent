55d656cbbd509f24e95245946906ed8258cfc136
Alpaca00 <alpaca00tuha@gmail.com>
2021-10-01 22:13:30 +0300

    behave fixture browser

---
+++ tests/scenarios/features/environment.py
@@ -0,0 +1,21 @@
+from behave import fixture, use_fixture
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@fixture
+def browser_chrome(context):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--maximized")
+    context.browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    yield context.browser
+    context.browser.quit()
+
+
+
+def before_tag(context, tag):
+    if tag == "fixture.browser.chrome":
+        use_fixture(browser_chrome, context)
+
+
