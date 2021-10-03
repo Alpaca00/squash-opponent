60369c5e2377f8073939d7bc180718b97acdba89
Alpaca00 <alpaca00tuha@gmail.com>
2021-10-03 10:16:55 +0300

    add skip if not correct language version of app

--- tests/scenarios/features/steps/user_input_invalid_password.py
+++ tests/scenarios/features/steps/user_input_invalid_password.py
@@ -1,3 +1,4 @@
+import re
 from behave import given, when, then
 from tests.scenarios.data_ import TEST_DATA
 
@@ -17,17 +18,27 @@ def launch_browser(context):
 @when('I enter valid email and invalid password "{invalid_password}"')
 def incorrect_password(context, invalid_password):
     user = context.user_login._open_url()
-    user._click(context.locator.navbar.btn_login)
-    user._input(
-        context.locator.login_page.email_field, TEST_DATA["valid"]["EMAIL"]
-    )
-    user._input(
-        context.locator.login_page.password_field, invalid_password
-    )
-    user._click(context.locator.login_page.submit_login)
+    lang_v = context.user_login.driver.current_url
+    res = [x for x in lang_v.split("/") if re.search(x, 'uk')]
+    if 'uk' not in res:
+        context.scenario.skip(reason="Not the Ukrainian version of the site.")
+    else:
+        user._click(context.locator.navbar.btn_login)
+        user._input(
+            context.locator.login_page.email_field, TEST_DATA["valid"]["EMAIL"]
+        )
+        user._input(
+            context.locator.login_page.password_field, invalid_password
+        )
+        user._click(context.locator.login_page.submit_login)
 
 
 @then('I will see flash message about "{message}" at login page')
 def flash_message(context, message):
-    alert_display = context.user_login._text(context.locator.login_page.flash_message)
-    assert alert_display == message
+    lang_v = context.user_login.driver.current_url
+    res = [x for x in lang_v.split("/") if re.search(x, 'uk')]
+    if 'uk' not in res:
+        context.scenario.skip(reason="Not the Ukrainian version of the site.")
+    else:
+        alert_display = context.user_login._text(context.locator.login_page.flash_message)
+        assert alert_display == message
--- tests/scenarios/features/steps/user_input_valid_login.py
+++ tests/scenarios/features/steps/user_input_valid_login.py
@@ -1,3 +1,4 @@
+import re
 from dataclasses import dataclass
 from behave import given, when, then
 from tests.locators.login_page_locators import LoginLocators
@@ -29,18 +30,23 @@ def open_url(context):
 @then("I will see the account details")
 def account_details(context):
     user = context.user_login._open_url()
-    user._click(context.locator.navbar.btn_login)
-    user._input(context.locator.login_page.email_field, TEST_DATA["valid"]["EMAIL"])
-    user._input(
-        context.locator.login_page.password_field, TEST_DATA["valid"]["USER_PASSWORD"]
-    )
-    user._click(context.locator.login_page.submit_login)
-    user_will_see_the_text = user._text_is_displayed(
-        selector=context.locator.user_account.email_info,
-        index_for_list=0,
-        text=TEST_DATA["valid"]["EMAIL"],
-    )
-    assert user_will_see_the_text
+    lang_v = context.user_login.driver.current_url
+    res = [x for x in lang_v.split("/") if re.search(x, 'uk')]
+    if 'uk' not in res:
+        context.scenario.skip(reason="Not the Ukrainian version of the site.")
+    else:
+        user._click(context.locator.navbar.btn_login)
+        user._input(context.locator.login_page.email_field, TEST_DATA["valid"]["EMAIL"])
+        user._input(
+            context.locator.login_page.password_field, TEST_DATA["valid"]["USER_PASSWORD"]
+        )
+        user._click(context.locator.login_page.submit_login)
+        user_will_see_the_text = user._text_is_displayed(
+            selector=context.locator.user_account.email_info,
+            index_for_list=0,
+            text=TEST_DATA["valid"]["EMAIL"],
+        )
+        assert user_will_see_the_text
 
 
 @then("close browser")
