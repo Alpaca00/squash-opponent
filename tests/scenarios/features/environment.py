81322ece1879955a72d11aad499375adbad68102
Alpaca00 <alpaca00tuha@gmail.com>
2021-10-03 10:15:16 +0300

    add mode

--- tests/scenarios/features/environment.py
+++ tests/scenarios/features/environment.py
@@ -6,12 +6,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 @fixture
 def browser_chrome(context):
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    mode = context.config.userdata.get("hidden", "headless")
+    if mode is not None:
+        options.add_argument("--headless")
     options.add_argument("--maximized")
     context.browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     yield context.browser
     context.browser.quit()
-
 
 
 def before_tag(context, tag):
