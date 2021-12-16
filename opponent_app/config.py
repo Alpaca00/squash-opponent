aeb151c09d802cc77aaa7b6c955ea0bc18e99917
Alpaca00 <alpaca00tuha@gmail.com>
2021-12-16 17:27:50 +0200

    Fix flask mail message

--- opponent_app/config.py
+++ opponent_app/config.py
@@ -42,9 +42,9 @@ configurations = {
 
 mail_settings = {
     "MAIL_SERVER": "smtp.gmail.com",
-    "MAIL_PORT": 465,
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
+    "MAIL_PORT": 587,
+    "MAIL_USE_TLS": True,
+    "MAIL_USE_SSL": False,
     "MAIL_USERNAME": os.environ["EMAIL_USERNAME"],
     "MAIL_PASSWORD": os.environ["EMAIL_PASSWORD"],
 }
