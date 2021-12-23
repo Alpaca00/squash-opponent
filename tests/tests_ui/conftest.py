235896d308f3440e14b5b3f84adaf2fbb78803eb
Alpaca00 <alpaca00tuha@gmail.com>
2021-12-23 11:54:34 +0200

    Change ip for connection

--- tests/tests_ui/conftest.py
+++ tests/tests_ui/conftest.py
@@ -3,11 +3,12 @@ import pytest
 import psycopg2 as pg
 
 PG_PASSWORD = os.environ["PG_PASSWORD"]
+HOST = os.environ['HOST']
 
 
 @pytest.fixture
 def connect_db():
-    conn = pg.connect(dbname="alpaca", user="user", password=PG_PASSWORD, host="46.101.139.62", port="5432")
+    conn = pg.connect(dbname="alpaca", user="user", password=PG_PASSWORD, host=HOST, port="5432")
     cur = conn.cursor()
     yield cur
     conn.commit()
