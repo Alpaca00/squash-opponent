83e73962514d68dd35b9ab4afa8c22b6b616a38c
Alpaca00 <alpaca00tuha@gmail.com>
2021-12-24 13:04:05 +0200

    after perform change it

--- tests/tests_ui/conftest.py
+++ tests/tests_ui/conftest.py
@@ -2,13 +2,13 @@ import os
 import pytest
 import psycopg2 as pg
 
-PG_PASSWORD = os.environ["PG_PASSWORD"]
+# PG_PASSWORD = os.environ["PG_PASSWORD"]
 HOST = os.environ['HOST']
 
 
 @pytest.fixture
 def connect_db():
-    conn = pg.connect(dbname="alpaca", user="user", password=PG_PASSWORD, host=HOST, port="5432")
+    conn = pg.connect(dbname="alpaca", user="user", password="password", host=HOST, port="5432")
     cur = conn.cursor()
     yield cur
     conn.commit()
