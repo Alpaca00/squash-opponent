193bc2bb51a4fcc3b9566a8a64d7d2b7d5d5ed79
Alpaca00 <alpaca00tuha@gmail.com>
2021-12-25 14:02:45 +0200

    Add cls AccountPage, methods for tab & autocomplete form for post

---
+++ tests/src/pages/account_page.py
@@ -0,0 +1,27 @@
+from tests.src import BasePage
+from tests.src.components.finder_opponent_tab import FinderOpponentTab
+from tests.src.elements.button import action
+
+
+class AccountPage(BasePage):
+    def __init__(self):
+        super().__init__()
+        self.anchor_element_loc = "//div[@id='page-content']"
+
+    @action('Tournaments')
+    def tournaments_tab(self):
+        if not self.browser.driver.current_url.endswith('/account/'):
+            self.browser.open(self.url + '/account/')
+        return self
+
+    @action('Finder opponent')
+    def finder_opponent_tab(self):
+        if not self.browser.driver.current_url.endswith('/account/'):
+            self.browser.open(self.url + '/account/')
+        return self
+
+    @action('POST')
+    def autocomplete_opponent_form(self, phone, district, category, datetime_):
+        FinderOpponentTab(phone, district, category, datetime_).fill_form()
+        return self
+
