de0a00062e150ef5e4adfba236b020be6c4b5158
Alpaca00 <alpaca00tuha@gmail.com>
2021-12-26 11:55:33 +0200

    Add test data for Tournaments in user account page

--- tests/tests_ui/tests_regression/data_tests/data_user_account.py
+++ tests/tests_ui/tests_regression/data_tests/data_user_account.py
@@ -8,3 +8,13 @@ class UserAccount:
         datetime_ = '2022-01-20T18:00'
         exp_res = 'Successfully. Your post has been added.'
         endpoint = '/account/'
+
+    class Tournament:
+        title = 'Lviv getshirty tour'
+        phone = '+380634329877'
+        district = 'Sychivskyi'
+        quantity_member = '7'
+        category = 'PRO'
+        datetime_ = '2022-01-20T19:00'
+        exp_res = 'Successfully. The tournament has been created.'
+        endpoint = '/account/tournaments'
