77ba570892af622fce7f1327b3abb42c4f72805c
Alpaca00 <alpaca00tuha@gmail.com>
2021-12-27 13:14:06 +0200

    Fix func 'before action'

--- tests/src/elements/button.py
+++ tests/src/elements/button.py
@@ -11,43 +11,41 @@ class Button(BasePage):
             f"//input[contains(@value, '{locator}')] |"
             f"//a[contains(text(), '{locator}')] |"
             f"//a[contains(., '{locator}')] |"
+            f"//span[contains(., '{locator}')] |"
             f"//button[contains(text(), '{locator}')] "
         )
 
-    def click(self) -> None:
-        self.element(self.locator).click()
+    def click(self, index=0) -> None:
+        self.element(self.locator, index=index).click()
 
 
 class ActionButton:
     def __init__(self, button_name: str) -> None:
         self.el = Button(button_name)
 
-    @property
-    def action(self) -> None:
-        self.el.click()
+    def action(self, index=0) -> None:
+        self.el.click(index=index)
 
 
-def action(button_name) -> Callable:
+def action(button_name, index=0) -> Callable:
 
     def decorator(func) -> Callable:
         @functools.wraps(func)
         def wrap_arguments(*args, **kwargs) -> ActionButton.action:
             func(*args, **kwargs)
-            return ActionButton(button_name).action
+            return ActionButton(button_name).action(index=index)
         return wrap_arguments
 
     return decorator
 
 
-def before_action(button_name) -> Callable:
+def before_action(button_name, index) -> Callable:
 
     def decorator(func) -> Callable:
         @functools.wraps(func)
-        def wrap_arguments(*args, **kwargs) -> Callable:
-            def local_wrap() -> ActionButton.action:
-                return ActionButton(button_name).action
-            func(*args, **kwargs)
-            return local_wrap
+        def wrap_arguments(*args, **kwargs) -> ActionButton.action:
+            ActionButton(button_name).action(index=index)
+            return func(*args, **kwargs)
         return wrap_arguments
 
     return decorator
