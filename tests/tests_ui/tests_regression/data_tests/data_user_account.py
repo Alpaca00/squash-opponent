b09d49a8d9b4ee8d13c487bb01c060c71166b117
Alpaca00 <alpaca00tuha@gmail.com>
2021-12-27 13:16:59 +0200

    Add test data for finder page

--- tests/tests_ui/tests_regression/data_tests/data_user_account.py
+++ tests/tests_ui/tests_regression/data_tests/data_user_account.py
@@ -18,3 +18,17 @@ class UserAccount:
         datetime_ = '2022-01-20T19:00'
         exp_res = 'Successfully. The tournament has been created.'
         endpoint = '/account/tournaments'
+
+    class FinderOpponent:
+        name = 'John Doe'
+        email = 'johndoetest0987@neo.com'
+        phone = '+380630000002'
+        district = 'Sychivskyi'
+        category = 'PRO'
+        message = '!hola amigo'
+        datetime_ = '2022-01-20T18:00'
+        exp_res = 'Successfully. Your post has been added.'
+        endpoint = '/finder/'
+
+    class ActionOpponent:
+        exp_res_delete = 'Successfully. Your post has been deleted.'
