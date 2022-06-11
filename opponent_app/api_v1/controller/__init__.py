fa417607d2f08319013cc4dcae8515fb4f622b62
Alpaca00 <alpaca00tuha@gmail.com>
2022-06-11 11:27:47 +0300

    add imports

--- opponent_app/api_v1/controller/__init__.py
+++ opponent_app/api_v1/controller/__init__.py
@@ -2,3 +2,4 @@ from .get_user_publications import get_user_publications_api
 from .get_all_publications import get_all_publications_api
 from .check_health_api import check_health
 from .get_all_tournaments import get_all_tournaments_api
+from .create_publication import create_publication_api
